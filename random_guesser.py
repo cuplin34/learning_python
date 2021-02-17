fa8af6962d1556e7e2522f72d0469a9974523c71
cuplin34 <38818055+cuplin34@users.noreply.github.com>
2021-02-17 11:22:36 -0800

    Update random_guesser.py

--- random_guesser.py
+++ random_guesser.py
@@ -1,14 +1,16 @@
 from random import randint
 
 game_state = "y"
+guess_count = 0
 while game_state == "y":
-    comp_guess = randint(1,10)
-    player_guess = int(input("Guess a number from 1 to 10\n"))
+    comp_guess = randint(1,100000)
+    player_guess = int(input("Guess a number from 1 to 100000\n"))
     while player_guess != comp_guess:
         if player_guess > comp_guess:
             print("Too high")
         else:   print("Too low")
+        guess_count += 1
         player_guess = int(input())
         
-    print(f"Wow you guessed {comp_guess} correctly")
+    print(f"Wow you guessed {comp_guess} correctly in {guess_count} guesses")
     game_state = input("Do you want to play again y/n?\n")

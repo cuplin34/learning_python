14d4f6883659f6acb6a9c311fadc137fb83f5897
cuplin34 <38818055+cuplin34@users.noreply.github.com>
2023-06-15 01:07:40 -0600

    Update cards.py
    Removed previous testing code

--- poker/cards.py
+++ poker/cards.py
@@ -1,8 +1,6 @@
 class Cards:
     deck = []
     #cards made in value suit model eg 9Diamonds ASpades
-    #def __init__(self, value):
-    #    self.value = value
     def add_deck(x):
         deck = x
 
@@ -10,10 +8,3 @@ class Cards:
         if "10" in self.value:
             return f"{self.value[0:2]} of {self.value[2:]}"
         return f"{self.value[0]} of {self.value[1:]}"
-
-#deck = []
-#for x in ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]:
-#    print(Cards((f"{x}Diamond")))
-#    print(Cards((f"{x}Spade")))
-#    print(Cards((f"{x}Heart")))
-#    print(Cards((f"{x}Clubs")))
